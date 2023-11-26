dfc0a3573648f83449b0e75086e27456b0e43ff7
Vivek <123234657+VivekCoderOp@users.noreply.github.com>
2023-11-26 13:31:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "6654918132").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/VivekNoobOp/NewThumb")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/kuchnhihai/reeder")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
